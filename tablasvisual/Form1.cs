8a44abd0208db2ca0f2556178a9ac2322aea01c6
KarlaVL <karla_98vl@gmail.com>
2018-03-13 16:43:02 -0600

    se agregó formularios a tablas categorias

---
+++ tablasvisual/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tablasvisual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void categoriasBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.categoriasBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.inventarioDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'inventarioDataSet.categorias' Puede moverla o quitarla según sea necesario.
+            this.categoriasTableAdapter.Fill(this.inventarioDataSet.categorias);
+
+        }
+    }
+}
